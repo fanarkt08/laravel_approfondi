78b544014d7503b84180f29e4431826d44569df7
fanarkt08 <fanarkt08@gmail.com>
2026-04-13 14:48:53 +0200

    update activity 2

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Order;
+use App\Models\User;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', 'test@example.com')->first();
+
+        Order::create([
+            'user_id' => $user->id,
+            'amount' => 50,
+            'status' => 'paid'
+        ]);
+    }
+}
