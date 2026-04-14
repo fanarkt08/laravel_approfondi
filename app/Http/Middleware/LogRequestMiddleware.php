d40a42e9d64e81fef2d65a935d763aa604282d06
fanarkt08 <fanarkt08@gmail.com>
2026-04-14 14:31:16 +0200

    update activity 5

--- app/Http/Middleware/LogRequestMiddleware.php
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -4,17 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class LogRequestMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        // Récupérer le User-Agent
+        $userAgent = $request->header('User-Agent');
+
+        // Log dans storage/logs/laravel.log
+        Log::info('User Agent detected', [
+            'user_agent' => $userAgent
+        ]);
+
         return $next($request);
     }
 }
